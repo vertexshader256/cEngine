1c24a1d81207f82602f31df61e3df48af190d8a8
bdwashbu <bdwashbu@gmail.com>
2017-05-09 16:01:02 -0500

    ArrayVariable use in subscript

--- app/Expressions.scala
+++ app/Expressions.scala
@@ -102,6 +102,9 @@ object Expressions {
           var aType = arrayVarPtr.theType
         
           val offset = aType match {
+            case ptrVar: ArrayVariable =>
+              aType = ptrVar.theType
+              (arrayVarPtr.address + 4) + index * 4
             case array: IArrayType if array.getType.isInstanceOf[IArrayType] =>
               aType = array.getType
               context.readPtrVal(arrayVarPtr.address).value.asInstanceOf[Int] + index * 4
