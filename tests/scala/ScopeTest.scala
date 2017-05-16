ca263185511046680e1b3dceacc27091dad780b2
bdwashbu <bdwashbu@gmail.com>
2017-05-16 07:14:56 -0500

    new scope test

--- tests/scala/ScopeTest.scala
+++ tests/scala/ScopeTest.scala
@@ -45,6 +45,32 @@ class ScopeTest extends StandardTest {
     checkResults(code)
   }
 
+  "function-scoped unitialized static vars" should "print the correct results" in {
+    val code = """
+
+      void test(int z)
+      {
+
+        static int x;
+
+        if (z == 5) {
+           x += 6;
+        }
+
+        printf("%d\n", x);
+      }
+
+      void main()
+      {
+        test(5);
+        test(6);
+        test(5);
+      }
+      """
+
+    checkResults(code)
+  }
+
   "function-scoped static vars overriding global" should "print the correct results" in {
     val code = """
 
