f0c0ed4ea4b593691c4422a4a9b8a0c33ae49474
bdwashbu <bdwashbu@gmail.com>
2017-05-25 14:03:52 -0500

    dont need this

--- src/scala/c_engine/Executor.scala
+++ src/scala/c_engine/Executor.scala
@@ -46,9 +46,6 @@ class ArrayVariable(name: String, state: State, arrayType: IArrayType, dim: Seq[
 
   override val theType = arrayType
 
-
-  override def value: RValue = RValue(address, theType)
-
   val allocate: Int = {
     // where we store the actual data
 
