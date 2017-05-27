6166cc9fb57ab1a35ecc811b5eaf68fe43cbf700
bdwashbu <bdwashbu@gmail.com>
2017-05-27 11:13:59 -0500

    removing Gcc error parsing stuff

--- src/scala/c/engine/Gcc.scala
+++ src/scala/c/engine/Gcc.scala
@@ -84,7 +84,7 @@ object Gcc {
     val compile = builder.run(logger.process)
     compile.exitValue()
     
-    val numErrors = logger.errors.filter(_.errorType == "error").length
+    val numErrors = logger.errors.length
 
     val result = if (numErrors == 0) {    
       val linkTokens = Seq("gcc") ++ Seq("-o", exeFile.path.toString) ++ objectFiles.map(_.path.toString)
@@ -106,7 +106,7 @@ object Gcc {
 
       runLogger.stdout
     } else {
-      logger.errors.filter(_.errorType == "error").map(_.error)
+      logger.errors
     }
     
     files.foreach{file => file.delete(true)}
@@ -114,98 +114,6 @@ object Gcc {
     exeFile.delete(true)
     
     result
-  } 
-  
-  def parseGccErrorString(error: String, function: String, path: Seq[String]): Option[BuildError] = {
-    try {
-      if ((error contains "error:") ||
-        (error contains "warning:") ||
-        (error contains "note:") ||
-        (error contains "In file included from")) {
-        
-        if (error.contains("In file included from")) {
-          val path = error.split("In file included from ").last
-        }
-  
-        if (error.contains(".c:")) {
-          val endOfFilePath = error.indexOf(".c:") + 2
-    
-          val remaining = error.substring(endOfFilePath + 1)
-    
-          val tokens = remaining.split(":")
-          
-          val file = error.substring(0, endOfFilePath)
-          val line = Integer.parseInt(tokens(0))
-    
-          val column = Integer.parseInt(tokens(1))
-          val errorType = tokens(2).trim
-          Some(BuildError(Seq(ErrorLocation(File(file), line, column)), Some(function), errorType, tokens.splitAt(3)._2.reduce(_ + ":" + _).trim))
-        } else if (path.isEmpty) { // immediate header issue
-          val endOfFilePath = error.indexOf(".h:") + 2
-    
-          val remaining = error.substring(endOfFilePath + 1)
-    
-          val tokens = remaining.split(":")
-          
-          val file = error.substring(0, endOfFilePath)
-          val line = Integer.parseInt(tokens(0))
-    
-          val column = Integer.parseInt(tokens(1))
-          val errorType = tokens(2).trim
-          Some(BuildError(Seq(ErrorLocation(File(file), line, column)), Some(function), errorType, tokens.splitAt(3)._2.reduce(_ + ":" + _).trim))
-        } else { // nested header issue
-          val endOfCFilePath = path.last.indexOf(".c:") + 2
-          val fromPos = path.last.indexOf(raw"C:\")
-    
-          val CfilePath = path.last.substring(fromPos, endOfCFilePath)
-          val Cremaining = path.last.substring(endOfCFilePath + 1)
-          val Ctokens = Cremaining.split(":")
-          val Cline = Integer.parseInt(Ctokens(0))
-          
-          val endOfFilePath = error.indexOf(".h:") + 2
-    
-          val remaining = error.substring(endOfFilePath + 1)
-    
-          val tokens = remaining.split(":")
-          val file = error.substring(0, endOfFilePath)
-          val line = Integer.parseInt(tokens(0))
-    
-          val errorLocations = path.reverse.flatMap{ location => 
-            if (location contains ".h:") {
-              val stripped = location.drop(location.indexOf("C:\\")).reverse.drop(1).reverse
-              val endOfFilePath = stripped.indexOf(".h:") + 2
-              val remaining = stripped.substring(endOfFilePath + 1)
-      
-              val tokens = remaining.split(":")
-              val file = stripped.substring(0, endOfFilePath)
-              val line = Integer.parseInt(tokens(0))
-        
-              //val column = Integer.parseInt(tokens(1))
-              //println("Z: " + ErrorLocation(File(file), line, column))
-              Some(ErrorLocation(File(file), line, 0))
-            } else {
-              None
-            }
-          }
-          
-          val column = Integer.parseInt(tokens(1))
-          val errorType = tokens(2).trim
-          val originatingCFile = File(CfilePath)
-          Some(BuildError(Seq(ErrorLocation(originatingCFile, Cline, column)) ++ errorLocations ++ Seq(ErrorLocation(File(file), line, column)), None, errorType, tokens.splitAt(3)._2.reduce(_ + ":" + _).trim))
-        }
-      } else if (error.contains("In function") && !error.contains("At top level:")) {
-        Some(BuildError(Seq(), Some(function), "Misc", error))
-      } else {
-        None
-      }
-    } catch {
-      case problem: Exception => {
-        println("ERROR: " + error)
-        println("PATH: " + path)
-        problem.printStackTrace()
-        None
-      }
-    }
   }
 }
 
@@ -215,9 +123,9 @@ case class BuildError(problemPath: Seq[ErrorLocation], function: Option[String],
 
 class SyntaxLogger extends Logger {
   
-  val errors = new ListBuffer[BuildError]()
+  val errors = new ListBuffer[String]()
 
-  def addErrors(newErrors: Seq[BuildError]) = {
+  def addErrors(newErrors: Seq[String]) = {
     errors ++= newErrors
   }
   
@@ -227,17 +135,6 @@ class SyntaxLogger extends Logger {
 
     val lines = scala.io.Source.fromInputStream(stream).getLines.toSeq
     val errors = getErrors(lines)
-
-    if (!errors.isEmpty) {
-
-      val fixedFileNameErrors = errors.flatMap { error =>
-        error.problemPath.headOption.map { err =>
-          error.copy(problemPath = Seq(ErrorLocation(File(err.file.path.toString.replace(".syntax", "")), err.line, err.column)))
-        }
-      }
-
-      addErrors(fixedFileNameErrors)
-    }
   }
 }
 
@@ -262,9 +159,9 @@ class RunLogger {
 
 class LinkerLogger extends Logger {
   
-  val errors = new ListBuffer[BuildError]()
+  val errors = new ListBuffer[String]()
 
-  def addErrors(newErrors: Seq[BuildError]) = {
+  def addErrors(newErrors: Seq[String]) = {
     errors ++= newErrors
   }
 
@@ -293,44 +190,9 @@ abstract class Logger {
   var isParsingSourcePath = false
   var currentFunction = ""
 
-  // Advance the state machine
-  def checkForErrors(error: String): Option[BuildError] = {
-    println(error)
-    if (error contains "In function") {
-      val start = error.indexOf("\'") + 1
-      val end = error.size - 2
-      currentFunction = error.substring(start, end)
-      None
-    } else if (error.contains("In file included from") && error.last == ',') {
-      // Some errors span multiple lines, in those cases were should accumulate it
-      isParsingSourcePath = true
-      errorSource.clear
-      errorSource += error
-      None
-    } else if ((error contains "from ") && error.last == ':') {
-      isParsingSourcePath = false
-      errorSource += error
-      None
-    } else if (isParsingSourcePath) {
-      errorSource += error
-      None
-    } else if (!error.contains("<command-line>")) {
-      val result = Gcc.parseGccErrorString(error, currentFunction, errorSource)
-      result
-    } else {
-      None
-    }
-  }
-
-  def addErrors(errors: Seq[BuildError])
+  def addErrors(errors: Seq[String])
   
-  def getErrors(lines: Seq[String]): Seq[BuildError] = {
-    val result = for {
-      errorString <- lines
-      error <- checkForErrors(errorString)
-    } yield error
-    
-    // just to be sure everything is unique.  Didnt want to use a set
-    result.distinct
+  def getErrors(lines: Seq[String]): Seq[String] = {
+    lines
   }
 }
