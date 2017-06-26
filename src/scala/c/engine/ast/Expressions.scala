9a17a6e302321b7d4c8d183ff8b0e896e07e27ae
bdwashbu <bdwashbu@gmail.com>
2017-06-26 18:25:42 -0500

    recursive version

--- src/scala/c/engine/ast/Expressions.scala
+++ src/scala/c/engine/ast/Expressions.scala
@@ -8,6 +8,160 @@ import scala.annotation.switch
 
 object Expressions {
 
+  def recurse(expr: IASTInitializerClause)(implicit context: State): Seq[ValueType] = expr match {
+    case exprList: IASTExpressionList =>
+      exprList.getExpressions.flatMap{x => recurse(x)}
+    case ternary: IASTConditionalExpression =>
+      val result = TypeHelper.resolveBoolean (recurse(ternary.getLogicalConditionExpression).head)
+
+      if (result) {
+        recurse(ternary.getPositiveResultExpression)
+      } else {
+        recurse(ternary.getNegativeResultExpression)
+      }
+    case cast: IASTCastExpression =>
+        val theType = TypeHelper.getType(cast.getTypeId).theType
+        val operand = recurse(cast.getOperand).head
+
+        Seq(operand match {
+          case str @ StringLiteral(_) => str
+          case LValue(addr, aType) =>
+            theType match {
+              case ptr: IPointerType if aType.isInstanceOf[IArrayType] =>
+                val newAddr = context.allocateSpace(4)
+                context.Stack.writeToMemory(addr, newAddr, theType)
+                LValue(newAddr, theType)
+              case _ => LValue(addr, theType)
+            }
+          case RValue(value, _) =>
+            val newAddr = context.allocateSpace(TypeHelper.sizeof(theType))
+            context.Stack.writeToMemory(TypeHelper.cast(theType, value).value, newAddr, theType)
+            LValue(newAddr, theType)
+        })
+    case fieldRef: IASTFieldReference =>
+        var baseAddr = -1
+
+        val struct = recurse(fieldRef.getFieldOwner).head.asInstanceOf[LValue]
+
+        def resolve(theType: IType, addr: Int): CStructure = theType match {
+          case qual: CQualifierType =>
+            resolve(qual.getType, addr)
+          case typedef: CTypedef =>
+            resolve(typedef.getType, addr)
+          case struct: CStructure => struct
+          case ptr: IPointerType =>
+            resolve(ptr.getType, baseAddr)
+        }
+
+        val structType = resolve(struct.theType, struct.address)
+
+        baseAddr = if (fieldRef.isPointerDereference) {
+          context.readPtrVal(struct.address)
+        } else {
+          struct.address
+        }
+
+        var offset = 0
+        var resultAddress: LValue = null
+        structType.getFields.foreach{field =>
+          if (field.getName == fieldRef.getFieldName.getRawSignature) {
+            // can assume names are unique
+            resultAddress = LValue(baseAddr + offset, field.getType)
+          } else {
+            offset += TypeHelper.sizeof(field.getType)
+          }
+        }
+
+        Seq(resultAddress)
+    case subscript: IASTArraySubscriptExpression =>
+        val index = recurse(subscript.getArgument).head match {
+          case x @ RValue(_, _) => TypeHelper.cast(TypeHelper.pointerType, x.value).value.asInstanceOf[Int]
+          case info @ LValue(_, _) =>
+            info.value.value match {
+              case int: Int => int
+              case long: Long => long.toInt
+            }
+        }
+
+        val arrayVarPtr = recurse(subscript.getArrayExpression).head.asInstanceOf[LValue]
+        var aType = arrayVarPtr.theType
+
+        val offset =
+          if (arrayVarPtr.isInstanceOf[ArrayVariable]) {
+            aType = arrayVarPtr.asInstanceOf[ArrayVariable].theType.getType
+            arrayVarPtr.address + index * TypeHelper.sizeof(aType)
+          } else {
+            aType match {
+              case array: IArrayType =>
+                aType = array.getType
+                context.readPtrVal(arrayVarPtr.address) + index * TypeHelper.sizeof(aType)
+              case ptr: IPointerType =>
+                aType = ptr.getType
+                context.readPtrVal(arrayVarPtr.address) + index * TypeHelper.sizeof(aType)
+            }
+          }
+
+        aType = TypeHelper.stripSyntheticTypeInfo(aType)
+
+        context.stack.push(LValue(offset, aType))
+
+        Seq()
+    case unary: IASTUnaryExpression =>
+      context.stack.push(UnaryExpression.execute(recurse(unary.getOperand).head, unary))
+    case lit: IASTLiteralExpression =>
+        val litStr = lit.getRawSignature
+        Seq(if (litStr.head == '\"' && litStr.last == '\"') {
+          StringLiteral(litStr)
+        } else {
+          Literal.cast(lit.getRawSignature)
+        })
+    case id: IASTIdExpression =>
+        Seq(context.context.resolveId(id.getName).get)
+    case typeExpr: IASTTypeIdExpression =>
+      // used for sizeof calls on a type
+        val theType = TypeHelper.getType(typeExpr.getTypeId).theType
+        Seq(RValue(TypeHelper.sizeof(theType), TypeHelper.pointerType))
+    case call: IASTFunctionCallExpression =>
+        val pop = recurse(call.getFunctionNameExpression).head
+
+        val name = if (context.hasFunction(call.getFunctionNameExpression.getRawSignature)) {
+          call.getFunctionNameExpression.getRawSignature
+        } else {
+          val info = pop.asInstanceOf[LValue]
+          val resolved = TypeHelper.stripSyntheticTypeInfo(info.theType)
+          resolved match {
+            case ptr: IPointerType => context.getFunctionByIndex(info.value.value.asInstanceOf[Int]).name
+          }
+        }
+
+        val args = call.getArguments.reverse.map{x => recurse(x).head}
+
+        val pushing = context.callTheFunction(name, call, args)
+        context.stack.pushAll(pushing)
+        Seq()
+    case bin: IASTBinaryExpression =>
+        val op2 = recurse(bin.getOperand2).head
+        val op1 = recurse(bin.getOperand1).head
+
+        val shouldShortCircuit = (bin.getOperator, op1) match {
+          case (IASTBinaryExpression.op_logicalOr, RValue(x: Boolean, _)) if x => true
+          case (IASTBinaryExpression.op_logicalAnd, RValue(x: Boolean, _)) if !x => true
+          case _ => false
+        }
+
+        val result = if (shouldShortCircuit) {
+          op1
+        } else {
+          if (Utils.isAssignment(bin.getOperator)) {
+            BinaryExpr.parseAssign(bin, bin.getOperator, op1, op2)
+          } else {
+            BinaryExpr.evaluate(bin, op1, op2, bin.getOperator)
+          }
+        }
+
+        Seq(result)
+  }
+
   def parse(expr: IASTExpression)(implicit context: State): PartialFunction[Direction, Seq[IASTNode]] = expr match {
     case exprList: IASTExpressionList => {
       case Stage2 => exprList.getExpressions
