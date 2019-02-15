0c51e2ed1b43aa5e5347c165e3c54677c5fd08ef
vertexshader <bdwashbu@gmail.com>
2019-02-15 17:21:29 -0600

    jpeg encoder and test

---
+++ tests/scala/c/engine/JpegTest.scala
@@ -0,0 +1,20 @@
+package scala.c.engine
+
+import java.nio.file.Paths
+
+import better.files.File
+
+class JpegTest extends StandardTest {
+  "jpeg test" should "print the correct results" in {
+    val code = """
+      void main() {
+        printf("nation emergency\n");
+      }"""
+
+    val slre = Paths.get("tests", "scala", "c", "engine", "jpeg", "jpeg_encoder.c")
+
+    val allCode =  Seq(File(slre).contentAsString)
+
+    checkResults2(allCode)
+  }
+}
