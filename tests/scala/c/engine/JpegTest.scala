32ef0c0855dd5b1d3603347d55053fc74517d353
vertexshader <bdwashbu@gmail.com>
2019-02-17 08:05:02 -0600

    added args to jpegTest

--- tests/scala/c/engine/JpegTest.scala
+++ tests/scala/c/engine/JpegTest.scala
@@ -5,16 +5,16 @@ import java.nio.file.Paths
 import better.files.File
 
 class JpegTest extends StandardTest {
-//  "jpeg test" should "print the correct results" in {
-//    val code = """
-//      void main() {
-//        printf("nation emergency\n");
-//      }"""
-//
-//    val slre = Paths.get("tests", "scala", "c", "engine", "jpeg", "jpeg_encoder.c")
-//
-//    val allCode =  Seq(File(slre).contentAsString)
-//
-//    checkResults2(allCode)
-//  }
+  "jpeg test" should "print the correct results" in {
+    val code = """
+      void main() {
+        printf("nation emergency\n");
+      }"""
+
+    val slre = Paths.get("tests", "scala", "c", "engine", "jpeg", "jpeg_encoder.c")
+
+    val allCode =  Seq(File(slre).contentAsString)
+
+    checkResults2(allCode, args = List("testImage.jpg", "10"))
+  }
 }
