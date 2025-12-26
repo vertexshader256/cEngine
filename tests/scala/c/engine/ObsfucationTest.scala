25864b83c46e7da8dc6c8deed60656b52f721f14
Brian Washburn <bdwash@gbinc.net>
2025-12-26 08:14:54 -0600

    fixed a test

--- tests/scala/c/engine/ObsfucationTest.scala
+++ tests/scala/c/engine/ObsfucationTest.scala
@@ -34,7 +34,7 @@ class ObsfucationTest extends StandardTest {
    #define e(x) --*x;
    #define d(x) --x;
    #define w(x) x x
-   main(){u *x=calloc(12,1);u *l=x;w(w(w(y(x))))w(y(x))v{p(x)w(w(y(x)))w(y(x))y(x)p
+   void main(){u *x=calloc(12,1);u *l=x;w(w(w(y(x))))w(y(x))v{p(x)w(w(y(x)))w(y(x))y(x)p
    (x)w(w(w(y(x))))w(y(x))p(x)w(y(x))y(x)p(x)w(w(w(y(x))))y(x)w(w(d(x)))e(x)}p(x)w(
    y(x))z(x)p(x)y(x)z(x)w(w(y(x)))w(y(x))y(x)w(z(x))w(y(x))y(x)z(x)p(x)w(y(x))z(x)p
    (x)w(e(x))e(x)z(x)w(d(x))z(x)w(y(x))y(x)z(x)w(w(e(x)))w(e(x))z(x)w(w(w(e(x))))z(
