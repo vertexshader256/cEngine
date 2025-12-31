8a197be07a42218151a3054f9373f3cb1e38b0f0
Brian Washburn <bdwash@gbinc.net>
2025-12-31 08:08:32 -0600

    refactoring

--- scala3/src/scala/c/engine/ast/Expressions.scala
+++ scala3/src/scala/c/engine/ast/Expressions.scala
@@ -30,67 +30,11 @@ object Expressions {
 
 			evaluate(expr)
 		case cast: IASTCastExpression =>
-			val theType = TypeHelper.getType(cast.getTypeId).theType
-			val operand = evaluate(cast.getOperand).get
-
-			Some(operand match {
-				case str@StringLiteral(_) => str
-				case LValue(addr, aType) =>
-					theType match {
-						case ptr: IPointerType if aType.isInstanceOf[IArrayType] =>
-							val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
-							state.Stack.writeToMemory(addr, newAddr, theType)
-							LValue(state, newAddr, theType)
-						case _ =>
-							val currentVal = state.Stack.readFromMemory(addr, aType) // read current variable value
-							val casted = TypeHelper.cast(theType, currentVal.value).value
-							val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
-							state.Stack.writeToMemory(casted, newAddr, theType) // write the casted data out
-							LValue(state, newAddr, theType)
-					}
-				case RValue(value, _) =>
-					val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
-					state.Stack.writeToMemory(TypeHelper.cast(theType, value).value, newAddr, theType)
-					LValue(state, newAddr, theType)
-			})
+			castExpression(cast)
 		case fieldRef: IASTFieldReference =>
-			val struct = evaluate(fieldRef.getFieldOwner).get.asInstanceOf[LValue]
-
-			val structType = TypeHelper.resolveStruct(struct.theType)
-
-			val baseAddr = if fieldRef.isPointerDereference then
-				state.readPtrVal(struct.address)
-			else
-				struct.address
-
-			val field = TypeHelper.offsetof(structType, baseAddr, fieldRef.getFieldName.toString, state: State)
-			Some(field)
+			fieldReference(fieldRef)
 		case subscript: IASTArraySubscriptExpression =>
-
-			var left = evaluate(subscript.getArrayExpression).get
-			var right = evaluate(subscript.getArgument).get
-
-			val isLeftPointer = TypeHelper.isPointerOrArray(left)
-
-			// in the case of weird stuff like 2[x], just swap the two operands
-			if !isLeftPointer then
-				val temp = left
-				left = right
-				right = temp
-
-			val base = TypeHelper.resolve(left).value.asInstanceOf[Int]
-
-			val indexType = left match
-				case RValue(_, theType) =>
-					theType
-				case LValue(_, theType) =>
-					TypeHelper.getPointerType(theType)
-
-			val rightValue = TypeHelper.resolve(right).value
-			val index = rightValue.toString.toInt
-			val offset = base + index * TypeHelper.sizeof(indexType)
-
-			Some(LValue(state, offset, indexType))
+			arraySubscriptExpression(subscript)
 		case unary: IASTUnaryExpression =>
 			Some(UnaryExpression.execute(unary))
 		case lit: IASTLiteralExpression =>
@@ -102,51 +46,130 @@ object Expressions {
 			val theType = TypeHelper.getType(typeExpr.getTypeId).theType
 			Some(RValue(TypeHelper.sizeof(theType), TypeHelper.intType))
 		case call: IASTFunctionCallExpression =>
-			val pop = evaluate(call.getFunctionNameExpression).head
-
-			val name = if (state.hasFunction(call.getFunctionNameExpression.getRawSignature)) {
-				call.getFunctionNameExpression.getRawSignature
-			} else {
-				val info = pop.asInstanceOf[LValue]
-				val resolved = TypeHelper.stripSyntheticTypeInfo(info.theType)
-				resolved match {
-					case _: IPointerType => state.getFunctionByIndex(info.rValue.value.asInstanceOf[Int]).name
-				}
-			}
-
-			state.callTheFunction(name, call, None)
+			functionCallExpr(call)
 		case bin: IASTBinaryExpression =>
-			(bin.getOperator, evaluate(bin.getOperand1).head) match {
-				case (IASTBinaryExpression.op_logicalOr, op1@RValue(x: Boolean, _)) if x => Some(op1)
-				case (IASTBinaryExpression.op_logicalAnd, op1@RValue(x: Boolean, _)) if !x => Some(op1)
-				case (_, op1) =>
-					val op2 = evaluate(bin.getOperand2).head
-
-					val result = if isAssignment(bin.getOperator) then
-						Declarator.assign(op1.asInstanceOf[LValue], List(op2), bin.getOperand2, bin.getOperator)
-					else
-						BinaryExpr.evaluate(op1, op2, bin.getOperator)
-
-					Some(result)
-			}
+			binaryExpression(bin)
 		case typeIdInit: IASTTypeIdInitializerExpression =>
-			val theType = TypeHelper.getType(typeIdInit.getTypeId).theType
-			val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
-
-			typeIdInit.getInitializer match {
-				case list: IASTInitializerList =>
-					val rVals = list.getClauses.map { clause =>
-						evaluate(clause).get match
-							case r@RValue(x, y) => r
-							case l: LValue => l.rValue
-					}.toList
-
-					state.writeDataBlock(rVals, newAddr)
-			}
-
-			Some(LValue(state, newAddr, theType))
+			typeExpr(typeIdInit)
 	}
 
+	private def castExpression(cast: IASTCastExpression)(implicit state: State) = {
+		val theType = TypeHelper.getType(cast.getTypeId).theType
+		val operand = evaluate(cast.getOperand).get
+
+		Some(operand match {
+			case str@StringLiteral(_) => str
+			case LValue(addr, aType) =>
+				theType match {
+					case ptr: IPointerType if aType.isInstanceOf[IArrayType] =>
+						val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
+						state.Stack.writeToMemory(addr, newAddr, theType)
+						LValue(state, newAddr, theType)
+					case _ =>
+						val currentVal = state.Stack.readFromMemory(addr, aType) // read current variable value
+						val casted = TypeHelper.cast(theType, currentVal.value).value
+						val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
+						state.Stack.writeToMemory(casted, newAddr, theType) // write the casted data out
+						LValue(state, newAddr, theType)
+				}
+			case RValue(value, _) =>
+				val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
+				state.Stack.writeToMemory(TypeHelper.cast(theType, value).value, newAddr, theType)
+				LValue(state, newAddr, theType)
+		})
+	}
+
+	private def arraySubscriptExpression(subscript: IASTArraySubscriptExpression)(implicit state: State) = {
+		var left = evaluate(subscript.getArrayExpression).get
+		var right = evaluate(subscript.getArgument).get
+
+		val isLeftPointer = TypeHelper.isPointerOrArray(left)
+
+		// in the case of weird stuff like 2[x], just swap the two operands
+		if !isLeftPointer then
+			val temp = left
+			left = right
+			right = temp
+
+		val base = TypeHelper.resolve(left).value.asInstanceOf[Int]
+
+		val indexType = left match
+			case RValue(_, theType) =>
+				theType
+			case LValue(_, theType) =>
+				TypeHelper.getPointerType(theType)
+
+		val rightValue = TypeHelper.resolve(right).value
+		val index = rightValue.toString.toInt
+		val offset = base + index * TypeHelper.sizeof(indexType)
+
+		Some(LValue(state, offset, indexType))
+	}
+
+	private def fieldReference(fieldRef: IASTFieldReference)(implicit state: State) = {
+		val struct = evaluate(fieldRef.getFieldOwner).get.asInstanceOf[LValue]
+
+		val structType = TypeHelper.resolveStruct(struct.theType)
+
+		val baseAddr = if fieldRef.isPointerDereference then
+			state.readPtrVal(struct.address)
+		else
+			struct.address
+
+		val field = TypeHelper.offsetof(structType, baseAddr, fieldRef.getFieldName.toString, state: State)
+		Some(field)
+	}
+
+	private def functionCallExpr(call: IASTFunctionCallExpression)(implicit state: State) = {
+		val pop = evaluate(call.getFunctionNameExpression).head
+
+		val name = if (state.hasFunction(call.getFunctionNameExpression.getRawSignature)) {
+			call.getFunctionNameExpression.getRawSignature
+		} else {
+			val info = pop.asInstanceOf[LValue]
+			val resolved = TypeHelper.stripSyntheticTypeInfo(info.theType)
+			resolved match {
+				case _: IPointerType => state.getFunctionByIndex(info.rValue.value.asInstanceOf[Int]).name
+			}
+		}
+
+		state.callTheFunction(name, call, None)
+	}
+
+	private def binaryExpression(bin: IASTBinaryExpression)(implicit state: State) = {
+		(bin.getOperator, evaluate(bin.getOperand1).head) match {
+			case (IASTBinaryExpression.op_logicalOr, op1@RValue(x: Boolean, _)) if x => Some(op1)
+			case (IASTBinaryExpression.op_logicalAnd, op1@RValue(x: Boolean, _)) if !x => Some(op1)
+			case (_, op1) =>
+				val op2 = evaluate(bin.getOperand2).head
+
+				val result = if isAssignment(bin.getOperator) then
+					Declarator.assign(op1.asInstanceOf[LValue], List(op2), bin.getOperand2, bin.getOperator)
+				else
+					BinaryExpr.evaluate(op1, op2, bin.getOperator)
+
+				Some(result)
+		}
+	}
+
+	private def typeExpr(typeIdInit: IASTTypeIdInitializerExpression)(implicit state: State) = {
+		val theType = TypeHelper.getType(typeIdInit.getTypeId).theType
+		val newAddr = state.allocateSpace(TypeHelper.sizeof(theType))
+
+		typeIdInit.getInitializer match {
+			case list: IASTInitializerList =>
+				val rVals = list.getClauses.map { clause =>
+					evaluate(clause).get match
+						case r@RValue(x, y) => r
+						case l: LValue => l.rValue
+				}.toList
+
+				state.writeDataBlock(rVals, newAddr)
+		}
+
+		Some(LValue(state, newAddr, theType))
+	}
+	
 	private def isAssignment(op: Int) = {
 		op == op_assign ||
 			op == op_plusAssign ||
