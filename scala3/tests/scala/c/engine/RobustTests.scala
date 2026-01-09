8b964bd53c7867d31501160852e88a6b7f4f37f2
Brian Washburn <bdwash@gbinc.net>
2026-01-08 23:12:37 -0600

    brazillian numbers

--- scala3/tests/scala/c/engine/RobustTests.scala
+++ scala3/tests/scala/c/engine/RobustTests.scala
@@ -133,4 +133,77 @@ class RobustTest extends StandardTest {
 
 		checkResults(code, args = List("fib1000.txt"))
 	}
+
+	"Brazilian numbers" should "print the correct results" in {
+		val code =
+			"""
+
+					int same_digits(int n, int b) {
+							int f = n % b;
+							n /= b;
+							while (n > 0) {
+									if (n % b != f) return 0;
+									n /= b;
+							}
+							return 1;
+					}
+
+					int is_brazilian(int n) {
+							int b;
+							if (n < 7) return 0;
+							if (!(n % 2) && n >= 8) return 1;
+							for (b = 2; b < n - 1; ++b) {
+									if (same_digits(n, b)) return 1;
+							}
+							return 0;
+					}
+
+					int is_prime(int n) {
+							int d = 5;
+							if (n < 2) return 0;
+							if (!(n % 2)) return n == 2;
+							if (!(n % 3)) return n == 3;
+							while (d * d <= n) {
+									if (!(n % d)) return 0;
+									d += 2;
+									if (!(n % d)) return 0;
+									d += 4;
+							}
+							return 1;
+					}
+
+					int main() {
+							int i, c, n;
+							const char *kinds[3] = {" ", " odd ", " prime "};
+							for (i = 0; i < 3; ++i) {
+									printf("First 15%sBrazilian numbers:\n", kinds[i]);
+									c = 0;
+									n = 7;
+									while (1) {
+											if (is_brazilian(n)) {
+													printf("%d ", n);
+													if (++c == 12) {
+															printf("\n\n");
+															break;
+													}
+											}
+											switch (i) {
+													case 0: n++; break;
+													case 1: n += 2; break;
+													case 2:
+															do {
+																	n += 2;
+															} while (!is_prime(n));
+															break;
+											}
+									}
+							}
+
+							return 0;
+					}
+
+					"""
+
+		checkResults(code)
+	}
 }
