81dcaf7e05313e0061a7c989f897b16d9f1cb21f
Brian Washburn <bdwash@gbinc.net>
2026-01-08 20:21:40 -0600

    1 test failing

--- scala3/src/scala/c/engine/Printf.scala
+++ scala3/src/scala/c/engine/Printf.scala
@@ -58,7 +58,6 @@ object Printf {
 							case _ =>
 								base
 
-						println("FORMAT STR: " + currentFormatString)
 						formatter2.format("%" + currentFormatString, List(convert) *)
 
 						var format: String = ""
@@ -180,7 +179,8 @@ object Printf {
 						else
 							"(null)".asInstanceOf[Object]
 
-						formatter2.format("%s", List(value) *)
+						println(currentFormatString)
+						formatter2.format("%" + currentFormatString + "s", List(value) *)
 						output.append(buffer2)
 
 						remainder = remainder.drop(1)
@@ -212,152 +212,6 @@ object Printf {
 				isDone = true
 			}
 		}
-
-		output.toString
-	}
-
-
-	def printf2(formattedOutputParams: Array[RValue], state: State): String = {
-		val str = Utils.readString(formattedOutputParams.last.value.asInstanceOf[Int])(using state)
-
-		var percentFound = false
-		var paramCount = 0
-
-		val resolved = new ListBuffer[Object]()
-
-		val varArgs = formattedOutputParams.reverse.tail.toList
-
-		def convertBoolean(): Object = {
-			val x = TypeHelper.toRValue(varArgs(paramCount))(using state).value
-			val convertedBool = x match
-				case bool: Boolean => if bool then 1 else 0
-				case _ => x
-
-			convertedBool.asInstanceOf[Object]
-		}
-
-		var resultingFormatString = ""
-		var formatFound = ""
-
-		if (str == "%llu\n") {
-			formatFound += "%s"
-
-			val bigInt = TypeHelper.toRValue(varArgs(paramCount))(using state).value
-			val longVal = Long.box(bigInt.asInstanceOf[Long])
-
-			resolved += java.lang.Long.toUnsignedString(Long.box(longVal))
-
-			resultingFormatString += formatFound
-
-			percentFound = false
-			paramCount += 1
-		} else {
-			str.toCharArray.foreach { c =>
-				if (!percentFound && c == '%') {
-					resultingFormatString += c
-					formatFound = ""
-					percentFound = true
-				} else if (percentFound && c == 's') {
-					percentFound = false
-					val theVal = varArgs(paramCount).value
-					val stringAddr = theVal.asInstanceOf[Int]
-
-					if stringAddr != 0 then
-						val str = Utils.readString(stringAddr)(using state)
-						resolved += str.split(System.lineSeparator()).mkString.asInstanceOf[Object]
-					else
-						resolved += "(null)".asInstanceOf[Object]
-
-					formatFound += c
-					resultingFormatString += formatFound
-					paramCount += 1
-				} else if (percentFound && (c == 'u')) {
-					formatFound += 'd'
-
-					val x = TypeHelper.toRValue(varArgs(paramCount))(using state).value
-
-					val value = x match
-						case long: Long => long & 0xFFFFFFFFL
-						case _ => TypeHelper.castToUnsigned(false, x)
-
-					resolved += value.asInstanceOf[Object]
-
-					resultingFormatString += 'd'
-					percentFound = false
-					paramCount += 1
-				} else if (percentFound && (c == 'd')) {
-					formatFound += c
-
-					if formatFound != "ld" && formatFound != "lld" then {
-						val num = TypeHelper.toRValue(varArgs(paramCount))(using state).value
-
-						num match
-							case long: Long => resolved +=
-								// trying to print a long with the %d format
-								Int.box(long.toInt)
-							case _ => resolved += convertBoolean()
-
-						resultingFormatString += formatFound
-					} else
-						resolved += convertBoolean()
-						resultingFormatString += 'd'
-
-					percentFound = false
-					paramCount += 1
-				} else if (percentFound && c == 'c') {
-					resolved += TypeHelper.toRValue(varArgs(paramCount))(using state).value.asInstanceOf[Object]
-					percentFound = false
-					formatFound += c
-					resultingFormatString += formatFound
-					paramCount += 1
-				} else if (percentFound && c == 'f') {
-					formatFound += c
-
-					val buffer2 = new StringBuffer()
-					val formatter2 = new Formatter(buffer2, Locale.US)
-
-					val base = TypeHelper.toRValue(varArgs(paramCount))(using state).value.asInstanceOf[Object]
-
-					val convert = base match
-						case boolean: java.lang.Boolean =>
-							if boolean then 1.0 else 0.0
-						case int: java.lang.Integer =>
-							int.toFloat
-						case _ =>
-							base
-
-					formatter2.format("%f", List(convert) *)
-
-					if (buffer2.toString.contains("Infinity") || buffer2.toString.contains("NaN")) {
-						resultingFormatString += 's'
-						resolved += buffer2.toString.replace("Infinity", "inf")
-							.replace("NaN", "-nan(ind)")
-					} else {
-						resultingFormatString += formatFound
-
-						base match
-							case boolean: java.lang.Boolean =>
-								val converted = if boolean then 1.0f else 0.0f
-								resolved += Float.box(converted)
-							case int: java.lang.Integer =>
-								resolved += Float.box(int.toFloat)
-							case _ =>
-								resolved += base
-					}
-
-					percentFound = false
-					paramCount += 1
-				} else if (percentFound) {
-					formatFound += c
-				} else {
-					resultingFormatString += c
-				}
-			}
-		}
-
-		val output = new StringBuffer()
-		val formatter = new Formatter(output, Locale.US)
-		formatter.format(resultingFormatString, resolved.toSeq *)
 
 		output.toString
 	}
