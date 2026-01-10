0095afc48a06ef5d4f80ce54874b288c2f25e2b7
Brian Washburn <bdwash@gbinc.net>
2026-01-10 15:44:26 -0600

    cleaned up pushing args on the stack for function calls

--- scala3/src/scala/c/engine/State.scala
+++ scala3/src/scala/c/engine/State.scala
@@ -365,16 +365,18 @@ class State(val sources: List[IASTTranslationUnit], val pointerSize: NumBits) {
 
 					val args: List[ValueType] = call.getArguments.map { x => Expressions.evaluate(x).head }.toList
 
-					val resolvedArgs = args.map(TypeHelper.toRValue)
+					args.foreach { argument =>
+						val resolved = TypeHelper.toRValue(argument)
 
-					// printf assumes all floating point numbers are doubles
-					val promoted = resolvedArgs.map: arg =>
-						arg.theType match
-							case basic: IBasicType if basic.getKind == IBasicType.Kind.eFloat => RValue(arg.value, TypeHelper.doubleType)
-							case _ => arg
+						// printf assumes all floating point numbers are doubles
+						val promoted = resolved.theType match
+							case basic: IBasicType if basic.getKind == IBasicType.Kind.eFloat => RValue(resolved.value, TypeHelper.doubleType)
+							case _ => resolved
 
-					newScope.pushOntoStack(promoted)
-					newScope.pushOntoStack(RValue(resolvedArgs.size, TypeHelper.unsignedIntType))
+						newScope.pushOntoStack(promoted)
+					}
+						
+					newScope.pushOntoStack(RValue(args.size, TypeHelper.unsignedIntType))
 
 					functionContexts = newScope +: functionContexts
 
