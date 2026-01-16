e63ffa1b2ab97745baec5b39227725ee8acd21b4
Brian Washburn <bdwash@gbinc.net>
2026-01-15 22:47:55 -0600

    helper methods on Address

--- scala3/src/scala/c/engine/models/Address.scala
+++ scala3/src/scala/c/engine/models/Address.scala
@@ -1,3 +1,6 @@
 package scala.c.engine.models
 
-case class Address(location: Int, segment: Memory)
+case class Address(location: Int, segment: Memory) {
+	def +(offset: Int) = Address(location + offset, segment)
+	def -(offset: Int) = Address(location - offset, segment)
+}
