47013390b4828900f925dbf22510c6f1405343c2
Brian Washburn <bdwash@gbinc.net>
2026-01-16 10:59:44 -0600

    adding writeToMemory to address

--- scala3/src/scala/c/engine/models/Address.scala
+++ scala3/src/scala/c/engine/models/Address.scala
@@ -1,6 +1,14 @@
 package scala.c.engine.models
 
+import org.eclipse.cdt.core.dom.ast.IType
+
+import scala.c.engine.cEngVal
+
 case class Address(location: Int, segment: Memory) {
 	def +(offset: Int) = Address(location + offset, segment)
 	def -(offset: Int) = Address(location - offset, segment)
+
+	def writeToMemory(newVal: cEngVal, theType: IType, bitOffset: Int = 0, sizeInBits: Int = 0) = {
+		segment.writeToMemory(newVal, location, theType)
+	}
 }
