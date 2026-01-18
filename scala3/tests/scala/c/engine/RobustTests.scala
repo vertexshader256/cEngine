7c8f92bbd6293a94021034a753e15517b147934e
Brian Washburn <bdwash@gbinc.net>
2026-01-17 20:12:40 -0600

    new test

--- scala3/tests/scala/c/engine/RobustTests.scala
+++ scala3/tests/scala/c/engine/RobustTests.scala
@@ -718,4 +718,34 @@ class RobustTest extends StandardTest {
 
 		checkResults(code)
 	}
+
+	"Repeat a string" should "print the correct results" in {
+		val code =
+			"""
+				#include <stdio.h>
+				#include <stdlib.h>
+				#include <string.h>
+
+				char * string_repeat( int n, const char * s ) {
+					size_t slen = strlen(s);
+					char * dest = malloc(n*slen+1);
+
+					int i; char * p;
+					for ( i=0, p = dest; i < n; ++i, p += slen ) {
+						memcpy(p, s, slen);
+					}
+					*p = '\0';
+					return dest;
+				}
+
+				int main() {
+					char * result = string_repeat(5, "ha");
+					puts(result);
+					free(result);
+					return 0;
+				}
+							"""
+
+		checkResults(code)
+	}
 }
