0c63c1e01a70b97e077c8d5f4881b95dc3e9a599
Brian Washburn <bdwash@gbinc.net>
2026-01-17 23:26:10 -0600

    IPParsetest is passing

--- scala3/tests/scala/c/engine/IPParseTest.scala
+++ scala3/tests/scala/c/engine/IPParseTest.scala
@@ -351,28 +351,28 @@ class IPParseTest extends StandardTest {
 					testcase ( "127.0.0.1" );
 
 					//The "localhost" IPv4 address, with a specified port (80)
-//					testcase ( "127.0.0.1:80" );
+					testcase ( "127.0.0.1:80" );
 					//The "localhost" IPv6 address
 					testcase ( "::1" );
 					//The "localhost" IPv6 address, with a specified port (80)
-//					testcase ( "[::1]:80" );
-//					//Rosetta Code's primary server's public IPv6 address
-//					testcase ( "2605:2700:0:3::4713:93e3" );
-//					//Rosetta Code's primary server's public IPv6 address, with a specified port (80)
-//					testcase ( "[2605:2700:0:3::4713:93e3]:80" );
-//
-//					//ipv4 space
-//					testcase ( "::ffff:192.168.173.22" );
-//					//ipv4 space with port
-//					testcase ( "[::ffff:192.168.173.22]:80" );
-//					//trailing compression
-//					testcase ( "1::" );
-//					//trailing compression with port
-//					testcase ( "[1::]:80" );
-//					//'any' address compression
-//					testcase ( "::" );
-//					//'any' address compression with port
-//					testcase ( "[::]:80" );
+					testcase ( "[::1]:80" );
+					//Rosetta Code's primary server's public IPv6 address
+					testcase ( "2605:2700:0:3::4713:93e3" );
+					//Rosetta Code's primary server's public IPv6 address, with a specified port (80)
+					testcase ( "[2605:2700:0:3::4713:93e3]:80" );
+
+					//ipv4 space
+					testcase ( "::ffff:192.168.173.22" );
+					//ipv4 space with port
+					testcase ( "[::ffff:192.168.173.22]:80" );
+					//trailing compression
+					testcase ( "1::" );
+					//trailing compression with port
+					testcase ( "[1::]:80" );
+					//'any' address compression
+					testcase ( "::" );
+					//'any' address compression with port
+					testcase ( "[::]:80" );
 
 					return 0;
 			}
