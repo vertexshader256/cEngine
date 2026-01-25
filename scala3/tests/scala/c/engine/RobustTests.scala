0f9fb1d9c1c218c421b4a211f129d1c4820a80cb
Brian Washburn <bdwash@gbinc.net>
2026-01-25 16:31:46 -0600

    new test

--- scala3/tests/scala/c/engine/RobustTests.scala
+++ scala3/tests/scala/c/engine/RobustTests.scala
@@ -1033,6 +1033,47 @@ class RobustTest extends StandardTest {
 		checkResults(code)
 	}
 
+	"gray number" should "print the correct results" in {
+		val code =
+			"""
+					#include <stdio.h>
+
+					int gray_encode(int n) {
+							return n ^ (n >> 1);
+					}
+
+					int gray_decode(int n) {
+							int p = n;
+							while (n >>= 1) p ^= n;
+							return p;
+					}
+
+					/* Simple bool formatter, only good on range 0..31 */
+					void fmtbool(int n, char *buf) {
+							char *b = buf + 5;
+							*b=0;
+							do {
+						*--b = '0' + (n & 1);
+						n >>= 1;
+							} while (b != buf);
+					}
+
+					void main(int argc, char **argv) {
+						int i,g,b;
+						char bi[6],bg[6],bb[6];
+
+						for (i=0 ; i<32 ; i++) {
+							g = gray_encode(i);
+							b = gray_decode(g);
+							fmtbool(i,bi); fmtbool(g,bg); fmtbool(b,bb);
+							printf("%2d : %5s => %5s => %5s : %2d\n", i, bi, bg, bb, b);
+						}
+					}
+						"""
+
+		checkResults(code)
+	}
+
 	// this test seems to use a lot of memory
 	"Pells equation" should "print the correct results" in {
 		val code =
