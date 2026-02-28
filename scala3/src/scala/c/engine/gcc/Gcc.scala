20855b8897de5e4ef9ac4ee96f9238ad230315c0
Brian Washburn <bdwash@gbinc.net>
2026-02-27 18:00:24 -0600

    making sure files get deleted

--- scala3/src/scala/c/engine/gcc/Gcc.scala
+++ scala3/src/scala/c/engine/gcc/Gcc.scala
@@ -32,84 +32,86 @@ object Gcc {
 			file
 		}
 
-		val moreIncludes = includePaths.flatMap { inc =>
-			Seq("-I", inc)
-		}
+		try {
+			val moreIncludes = includePaths.flatMap { inc =>
+				Seq("-I", inc)
+			}
 
-		val sourceFileTokens = files.flatMap { file => Seq(file.getAbsolutePath) }
-		val includeTokens = Seq("-I", Utils.mainPath) ++ moreIncludes
+			val sourceFileTokens = files.flatMap { file => Seq(file.getAbsolutePath) }
+			val includeTokens = Seq("-I", Utils.mainPath) ++ moreIncludes
 
-		val size = pointerSize match {
-			case ThirtyTwoBits => Seq("gcc")
-			case SixtyFourBits => Seq("gcc")
-		}
+			val size = pointerSize match {
+				case ThirtyTwoBits => Seq("gcc")
+				case SixtyFourBits => Seq("gcc")
+			}
 
-		val processTokens =
-			size ++ sourceFileTokens ++ includeTokens ++ Seq("-o", exeFile.getAbsolutePath) ++ Seq("-D", "ALLOC_TESTING")
+			val processTokens =
+				size ++ sourceFileTokens ++ includeTokens ++ Seq("-o", exeFile.getAbsolutePath) ++ Seq("-D", "ALLOC_TESTING")
 
-		val builder = Process(processTokens, java.io.File("."))
-		val compile = builder.run(logger.process)
+			val builder = Process(processTokens, java.io.File("."))
+			val compile = builder.run(logger.process)
 
-		compile.exitValue()
+			compile.exitValue()
 
-		logger.errors.toList.foreach(println)
+			logger.errors.toList.foreach(println)
 
-		val numErrors = 0 //logger.errors.length
+			val numErrors = 0 //logger.errors.length
 
-		val gccOutput = if (numErrors == 0) {
+			val gccOutput = if (numErrors == 0) {
 
-			var isDone = false
-			val maxTries = 50 // 50 is proven to work
-			var i = 0
-			var result: Seq[String] = null
+				var isDone = false
+				val maxTries = 50 // 50 is proven to work
+				var i = 0
+				var result: Seq[String] = null
 
-			Thread.sleep(30)
+				Thread.sleep(30)
 
-			// 3/1/19: Protip - This helps tests run reliably!
-			while (!isDone && i < maxTries) {
+				// 3/1/19: Protip - This helps tests run reliably!
+				while (!isDone && i < maxTries) {
 
-				i += 1
-				try {
-					val runLogger = new RunLogger
-					// run the actual executable
-					val path = Seq(exeFile.getAbsolutePath) ++ args
-					val runner = Process(path, new File("."))
-					val run = runner.run(runLogger.process)
+					i += 1
+					try {
+						val runLogger = new RunLogger
+						// run the actual executable
+						val path = Seq(exeFile.getAbsolutePath) ++ args
+						val runner = Process(path, new File("."))
+						val run = runner.run(runLogger.process)
 
-					run.exitValue()
+						run.exitValue()
 
-					result = runLogger.stdout.clone().toList
+						result = runLogger.stdout.clone().toList
 
-					if (result.nonEmpty) {
-						isDone = true
-						exeFile.delete()
+						if (result.nonEmpty) {
+							isDone = true
+						}
+					} catch {
+						case e: Throwable => Thread.sleep(50)
 					}
-				} catch {
-					case e: Throwable => Thread.sleep(50)
 				}
-			}
 
-			// delete the .c files
-			Try(files.foreach(_.delete()))
-
-			GccOutput(result, true)
-		} else {
-			GccOutput(logger.errors.toSeq, false)
-		}
-
-		if gccOutput != null then {
-
-			if (gccOutput.output != null) {
-				val hasNoCompileError = !gccOutput.output.exists(x => x.contains("returned 1 exit status"))
-
-				if hasNoCompileError then
-					gccOutput
-				else
-					GccOutput(gccOutput.output, false)
+				GccOutput(result, true)
 			} else {
-				GccOutput(Seq("Compilation error"), false)
+				GccOutput(logger.errors.toSeq, false)
 			}
-		} else
-			GccOutput(logger.errors.toSeq, false)
+
+			if gccOutput != null then {
+
+				if (gccOutput.output != null) {
+					val hasNoCompileError = !gccOutput.output.exists(x => x.contains("returned 1 exit status"))
+
+					if hasNoCompileError then
+						gccOutput
+					else
+						GccOutput(gccOutput.output, false)
+				} else {
+					GccOutput(Seq("Compilation error"), false)
+				}
+			} else
+				GccOutput(logger.errors.toSeq, false)
+		} finally {
+			// delete the .c files
+			Try(exeFile.delete())
+			Try(files.foreach(_.delete()))
+		}
 	}
 }
