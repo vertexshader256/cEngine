d1e6e28789fb9aaa5925c32e8b1e5adcca6b4fcd
Brian Washburn <bdwash@gbinc.net>
2026-02-27 22:38:34 -0600

    coverage

--- scala3/src/scala/c/engine/cFunctions/Stdargh.scala
+++ scala3/src/scala/c/engine/cFunctions/Stdargh.scala
@@ -18,43 +18,43 @@ object Stdargh {
 
 	def addFunctions(scalaFunctions: ListBuffer[Function]) = {
 
-		scalaFunctions += new EmulatedFunction("va_arg") {
-			def run(formattedOutputParams: Array[RValue], state: CEngine): Option[RValue] = {
-				val argTypeStr = formattedOutputParams(0).value.asInstanceOf[Int]
-
-				val str = Utils.readString(Address(argTypeStr))(using state)
-
-				val (offset, theType) = str match {
-					case "unsigned int" => (4, TypeHelper.unsignedIntType)
-					case "int" => (4, TypeHelper.intType)
-					case "double" => (8, TypeHelper.doubleType)
-					case "char" => (1, TypeHelper.charType)
-					case "char *" => (4, CPointerType(TypeHelper.charType, 0))
-					case "unsigned long" => (8, CPointerType(CBasicType(IBasicType.Kind.eInt, IBasicType.IS_LONG), 0))
-				}
-
-				val current = state.varArgStartingAddr.head
-				state.varArgStartingAddr = state.varArgStartingAddr.tail
-				val result = state.memory.readFromMemory(Address(current), theType).value
-				state.varArgStartingAddr = (current + offset) +: state.varArgStartingAddr
-
-				Some(RValue(result))
-			}
-		}
-
-		scalaFunctions += new EmulatedFunction("va_start") {
-			def run(formattedOutputParams: Array[RValue], state: CEngine): Option[RValue] = {
-				val lastNamedArgAddr = formattedOutputParams(0).value.asInstanceOf[Int]
-				state.varArgStartingAddr = (lastNamedArgAddr + 4) +: state.varArgStartingAddr
-				None
-			}
-		}
-
-		scalaFunctions += new EmulatedFunction("va_end") {
-			def run(formattedOutputParams: Array[RValue], state: CEngine): Option[RValue] = {
-				state.varArgStartingAddr = state.varArgStartingAddr.tail
-				None
-			}
-		}
+//		scalaFunctions += new EmulatedFunction("va_arg") {
+//			def run(formattedOutputParams: Array[RValue], state: CEngine): Option[RValue] = {
+//				val argTypeStr = formattedOutputParams(0).value.asInstanceOf[Int]
+//
+//				val str = Utils.readString(Address(argTypeStr))(using state)
+//
+//				val (offset, theType) = str match {
+//					case "unsigned int" => (4, TypeHelper.unsignedIntType)
+//					case "int" => (4, TypeHelper.intType)
+//					case "double" => (8, TypeHelper.doubleType)
+//					case "char" => (1, TypeHelper.charType)
+//					case "char *" => (4, CPointerType(TypeHelper.charType, 0))
+//					case "unsigned long" => (8, CPointerType(CBasicType(IBasicType.Kind.eInt, IBasicType.IS_LONG), 0))
+//				}
+//
+//				val current = state.varArgStartingAddr.head
+//				state.varArgStartingAddr = state.varArgStartingAddr.tail
+//				val result = state.memory.readFromMemory(Address(current), theType).value
+//				state.varArgStartingAddr = (current + offset) +: state.varArgStartingAddr
+//
+//				Some(RValue(result))
+//			}
+//		}
+//
+//		scalaFunctions += new EmulatedFunction("va_start") {
+//			def run(formattedOutputParams: Array[RValue], state: CEngine): Option[RValue] = {
+//				val lastNamedArgAddr = formattedOutputParams(0).value.asInstanceOf[Int]
+//				state.varArgStartingAddr = (lastNamedArgAddr + 4) +: state.varArgStartingAddr
+//				None
+//			}
+//		}
+//
+//		scalaFunctions += new EmulatedFunction("va_end") {
+//			def run(formattedOutputParams: Array[RValue], state: CEngine): Option[RValue] = {
+//				state.varArgStartingAddr = state.varArgStartingAddr.tail
+//				None
+//			}
+//		}
 	}
 }
