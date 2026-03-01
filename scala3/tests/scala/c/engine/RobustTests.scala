29133b5b07d37c2fd3b72b154ac24459118b8f2e
Brian Washburn <bdwash@gbinc.net>
2026-03-01 17:06:39 -0600

    new test

--- scala3/tests/scala/c/engine/RobustTests.scala
+++ scala3/tests/scala/c/engine/RobustTests.scala
@@ -1074,6 +1074,64 @@ class RobustTest extends StandardTest {
 		checkResults(code)
 	}
 
+	"attractive number" should "print the correct results" in {
+		val code =
+			"""
+				#include <stdio.h>
+
+				#define TRUE 1
+				#define FALSE 0
+				#define MAX 120
+
+				typedef int boolean;
+
+				boolean is_prime(int n) {
+						int d = 5;
+						if (n < 2) return FALSE;
+						if (!(n % 2)) return n == 2;
+						if (!(n % 3)) return n == 3;
+						while (d *d <= n) {
+								if (!(n % d)) return FALSE;
+								d += 2;
+								if (!(n % d)) return FALSE;
+								d += 4;
+						}
+						return TRUE;
+				}
+
+				int count_prime_factors(int n) {
+						int count = 0, f = 2;
+						if (n == 1) return 0;
+						if (is_prime(n)) return 1;
+						while (TRUE) {
+								if (!(n % f)) {
+										count++;
+										n /= f;
+										if (n == 1) return count;
+										if (is_prime(n)) f = n;
+								}
+								else if (f >= 3) f += 2;
+								else f = 3;
+						}
+				}
+
+				int main() {
+						int i, n, count = 0;
+						printf("The attractive numbers up to and including %d are:\n", MAX);
+						for (i = 1; i <= MAX; ++i) {
+								n = count_prime_factors(i);
+								if (is_prime(n)) {
+										printf("%4d", i);
+										if (!(++count % 20)) printf("\n");
+								}
+						}
+						printf("\n");
+						return 0;
+				}
+					"""
+		checkResults(code)
+	}
+
 	// this test seems to use a lot of memory
 	"Pells equation" should "print the correct results" in {
 		val code =
